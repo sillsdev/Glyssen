5f92b9b0368bec3199eb417847587382da88b920
Andrew Polk <andrew_polk@sil.org>
2014-11-12 11:06:05 -0500

    Add buildUpdate.sh

---
+++ build/buildUpdate.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# server=build.palaso.org
+# build_type=bt431
+# root_dir=..
+# $Id: d32984f53cd52f171a9cba46cd3879538ad23431 $
+
+cd "$(dirname "$0")"
+
+# *** Functions ***
+force=0
+clean=0
+
+while getopts fc opt; do
+case $opt in
+f) force=1 ;;
+c) clean=1 ;;
+esac
+done
+
+shift $((OPTIND - 1))
+
+copy_auto() {
+if [ "$clean" == "1" ]
+then
+echo cleaning $2
+rm -f ""$2""
+else
+where_curl=$(type -P curl)
+where_wget=$(type -P wget)
+if [ "$where_curl" != "" ]
+then
+copy_curl $1 $2
+elif [ "$where_wget" != "" ]
+then
+copy_wget $1 $2
+else
+echo "Missing curl or wget"
+exit 1
+fi
+fi
+}
+
+copy_curl() {
+echo "curl: $2 <= $1"
+if [ -e "$2" ] && [ "$force" != "1" ]
+then
+curl -# -L -z $2 -o $2 $1
+else
+curl -# -L -o $2 $1
+fi
+}
+
+copy_wget() {
+echo "wget: $2 <= $1"
+f=$(basename $2)
+d=$(dirname $2)
+cd $d
+wget -q -L -N $1
+cd -
+}
+
+
+# *** Results ***
+# build: Protoscript Generator-win-default Continuous (bt431)
+# project: Protoscript Generator
+# URL: http://build.palaso.org/viewType.html?buildTypeId=bt431
+# VCS: https://github.com/sillsdev/ProtoScriptGenerator.git [master]
+# dependencies:
+# [0] build: palaso-win32-master Continuous (bt223)
+#     project: libpalaso
+#     URL: http://build.palaso.org/viewType.html?buildTypeId=bt223
+#     clean: false
+#     revision: latest.lastSuccessful
+#     paths: {"Palaso.BuildTasks.dll"=>"build/", "icu.net.dll"=>"lib/dotnet", "icudt40.dll"=>"lib/dotnet", "icuin40.dll"=>"lib/dotnet", "icuuc40.dll"=>"lib/dotnet", "Ionic.Zip.dll"=>"lib/dotnet", "L10NSharp.dll"=>"lib/dotnet", "Palaso.dll"=>"lib/dotnet", "PalasoUIWindowsForms.dll"=>"lib/dotnet"}
+#     VCS: https://github.com/sillsdev/libpalaso.git []
+
+# make sure output directories exist
+mkdir -p ../build/
+mkdir -p ../lib/dotnet
+
+# download artifact dependencies
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/Palaso.BuildTasks.dll ../build/Palaso.BuildTasks.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/icu.net.dll ../lib/dotnet/icu.net.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/icudt40.dll ../lib/dotnet/icudt40.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/icuin40.dll ../lib/dotnet/icuin40.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/icuuc40.dll ../lib/dotnet/icuuc40.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/Ionic.Zip.dll ../lib/dotnet/Ionic.Zip.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/L10NSharp.dll ../lib/dotnet/L10NSharp.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/Palaso.dll ../lib/dotnet/Palaso.dll
+copy_auto http://build.palaso.org/guestAuth/repository/download/bt223/latest.lastSuccessful/PalasoUIWindowsForms.dll ../lib/dotnet/PalasoUIWindowsForms.dll
+# End of script
