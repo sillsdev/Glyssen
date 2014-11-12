7d83a24fa81944daed93c701a7cd3698a0f9113a
Andrew Polk <andrew_polk@sil.org>
2014-11-12 10:11:45 -0500

    Add Select Bundle dialog

---
+++ ProtoScript/ProtoScript/Dialogs/SelectBundleDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProtoScript.Dialogs
+{
+	class SelectBundleDialog : IDisposable
+	{
+		private const string kResourceBundleExtension = ".bun";
+		private readonly OpenFileDialog m_fileDialog;
+
+		public SelectBundleDialog()
+		{
+			m_fileDialog = new OpenFileDialog
+			{
+				InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+				Filter = "Bundle files|*" + kResourceBundleExtension
+			};
+		}
+
+		public void ShowDialog()
+		{
+			if (m_fileDialog.ShowDialog() == DialogResult.OK)
+				FileName = m_fileDialog.FileName;
+		}
+
+		public string FileName { get; private set; }
+
+		public void Dispose()
+		{
+			m_fileDialog.Dispose();
+		}
+	}
+}
---
+++ ProtoScript/ProtoScript/SandboxForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using ProtoScript.Dialogs;
+
+namespace ProtoScript
+{
+	public partial class SandboxForm : Form
+	{
+		public SandboxForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (var dlg = new SelectBundleDialog())
+			{
+				dlg.ShowDialog();
+				label1.Text = dlg.FileName;
+			}
+		}
+	}
+}
