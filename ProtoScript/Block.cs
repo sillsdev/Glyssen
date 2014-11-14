2f718d287f95153eae68f8f5066c18373ccfbf55
Tom Bogle <tom_bogle@sil.org>
2014-11-14 09:06:48 -0500

    Added classes to parse USX data into blocks and serialize results to XML.

---
+++ ProtoScript/Block.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Palaso.Xml;
+
+namespace ProtoScript
+{
+	[XmlRoot("block")]
+	public class Block
+	{
+		public Block()
+		{
+			// Needed for deserialization
+		}
+
+		public Block(string styleTag)
+		{
+			StyleTag = styleTag;
+			BlockElements = new List<BlockElement>();
+		}
+
+		[XmlAttribute("style")]
+		public string StyleTag { get; set; }
+
+		[XmlAttribute("characterId")]
+		public int CharacterId { get; set; }
+
+		//[XmlArrayItem(Type = typeof(ScriptText), ElementName = "text")]
+		//[XmlArrayItem(Type = typeof(Verse), ElementName = "verse")]
+		[XmlElement(Type = typeof(ScriptText), ElementName = "text")]
+		[XmlElement(Type = typeof(Verse), ElementName = "verse")]
+		public List<BlockElement> BlockElements { get; set; }
+
+		public string GetText(bool includeVerseNumbers)
+		{
+			StringBuilder bldr = new StringBuilder();
+			foreach (var blockElement in BlockElements)
+			{
+				Verse verse = blockElement as Verse;
+				if (verse != null)
+				{
+					if (includeVerseNumbers)
+					{
+						bldr.Append("[");
+						bldr.Append(verse.Number);
+						bldr.Append("]");
+					}
+				}
+				else
+				{
+					ScriptText text = blockElement as ScriptText;
+					if (text != null)
+						bldr.Append(text.Content);
+				}
+			}
+			return bldr.ToString();
+		}
+
+		[XmlIgnore]
+		public bool IsNarrator
+		{
+			get { return CharacterId == 0; }
+		}
+
+		public string GetAsXml()
+		{
+			return XmlSerializationHelper.SerializeToString(this);
+		}
+	}
+
+	[XmlInclude(typeof(ScriptText))]
+	[XmlInclude(typeof(Verse))]
+	public abstract class BlockElement
+	{
+	}
+
+	public class ScriptText : BlockElement
+	{
+		public ScriptText()
+		{
+			// Needed for deserialization
+		}
+
+		public ScriptText(string content)
+		{
+			Content = content;
+		}
+
+		[XmlText]
+		public string Content { get; set; }
+	}
+
+	public class Verse : BlockElement
+	{
+		public Verse()
+		{
+			// Needed for deserialization
+		}
+
+		public Verse(string number)
+		{
+			Number = number;
+		}
+
+		[XmlAttribute("num")]
+		public string Number{ get; set; }
+	}
+}
---
+++ ProtoScriptTests/BlockTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using ProtoScript;
+
+namespace ProtoScriptTests
+{
+	[TestFixture]
+	class BlockTests
+	{
+		[Test]
+		public void Serialization_VerseAndTextElements_Works()
+		{
+			var block = new Block("p");
+			block.BlockElements.Add(new Verse("1"));
+			block.BlockElements.Add(new ScriptText("Text of verse one."));
+			block.BlockElements.Add(new Verse("2"));
+			block.BlockElements.Add(new ScriptText("Text of verse two."));
+
+			Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-16\"?><block style=\"p\" characterId=\"0\">" +
+				"<verse num=\"1\"/>" +
+				"<text>Text of verse one.</text>" +
+				"<verse num=\"2\"/>" +
+				"<text>Text of verse two.</text>" +
+				"</block>",
+				block.GetAsXml().Replace("\r", string.Empty).Replace("\n", string.Empty).Replace("\t", string.Empty).Replace(" />", "/>"));
+		}
+	}
+}
