78024ce7160fc7da73d5db6c9f779742ea31d2b6
Tom Bogle <tom_bogle@sil.org>
2014-11-20 11:29:17 -0500

    Updated Parser Version # (forgot to do this with previous check-in)

--- ProtoScript/Properties/Settings.Designer.cs
+++ ProtoScript/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace ProtoScript.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("6")]
         public string PgUsxParserVersion {
             get {
                 return ((string)(this["PgUsxParserVersion"]));
