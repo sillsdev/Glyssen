fa23f20bc232bab233a89899aae45c274e46da90
Andrew Polk <andrew_polk@sil.org>
2015-01-12 10:47:44 -0500

    Increase parser version

--- ProtoScript/Properties/Settings.Designer.cs
+++ ProtoScript/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace ProtoScript.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("19")]
+        [global::System.Configuration.DefaultSettingValueAttribute("20")]
         public string PgUsxParserVersion {
             get {
                 return ((string)(this["PgUsxParserVersion"]));
