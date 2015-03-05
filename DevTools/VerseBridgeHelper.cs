4bae0c6e93fcb7c782b32c30fbb459029b69ae51
Andrew Polk <andrew_polk@sil.org>
2015-03-05 13:50:51 -0500

    Remove verse bridges so we can reincorporate changes from Jim

--- DevTools/VerseBridgeHelper.cs
+++ DevTools/VerseBridgeHelper.cs
@@ -25,16 +25,11 @@ namespace DevTools
 				sb.Append(cv.BookCode).Append(TAB)
 					.Append(cv.Chapter).Append(TAB).Append(cv.Verse).Append(TAB).Append(cv.Character).Append(TAB)
 					.Append(cv.Delivery).Append(TAB).Append(cv.Alias).Append(TAB)
-					.Append(DialogueOutput(cv.IsDialogue)).Append(TAB).Append(cv.DefaultCharacter).Append(TAB).Append(cv.ParallelPassageReferences)
+					.Append(cv.QuoteType).Append(TAB).Append(cv.DefaultCharacter).Append(TAB).Append(cv.ParallelPassageReferences)
 					.Append(Environment.NewLine);
 			}
 
 			File.WriteAllText(Path.Combine(CharacterListProcessing.kBaseDirForRealOutput, "CharacterVerse.txt"), sb.ToString());
-		}
-
-		private static string DialogueOutput(bool isDialogue)
-		{
-			return isDialogue ? "TRUE" : "FALSE";
 		}
 	}
 }
