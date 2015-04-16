0580ae587de527f691393ed1dd823ec7046c77ac
Andrew Polk <andrew_polk@sil.org>
2015-04-16 13:43:32 -0400

    Remove Paratext Project link and modify create new project link (PG-197)

--- ProtoScript/Dialogs/OpenProjectDlg.Designer.cs
+++ ProtoScript/Dialogs/OpenProjectDlg.Designer.cs
@@ -29,35 +29,17 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			System.Windows.Forms.Label lblNewProject;
 			this.m_l10NSharpExtender = new L10NSharp.UI.L10NSharpExtender(this.components);
 			this.lblExistingProject = new System.Windows.Forms.Label();
 			this.m_linkTextReleaseBundle = new System.Windows.Forms.LinkLabel();
 			this.m_btnOk = new System.Windows.Forms.Button();
 			this.m_btnCancel = new System.Windows.Forms.Button();
-			this.m_linkParatextProject = new System.Windows.Forms.LinkLabel();
 			this.m_chkShowInactiveProjects = new System.Windows.Forms.CheckBox();
-			this.m_tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
 			this.m_listExistingProjects = new ProtoScript.Controls.ExistingProjectsList();
-			lblNewProject = new System.Windows.Forms.Label();
+			this.m_tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
 			((System.ComponentModel.ISupportInitialize)(this.m_l10NSharpExtender)).BeginInit();
 			this.m_tableLayoutPanelMain.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// lblNewProject
-			// 
-			lblNewProject.AutoSize = true;
-			lblNewProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
-			lblNewProject.ForeColor = System.Drawing.Color.White;
-			this.m_l10NSharpExtender.SetLocalizableToolTip(lblNewProject, null);
-			this.m_l10NSharpExtender.SetLocalizationComment(lblNewProject, null);
-			this.m_l10NSharpExtender.SetLocalizingId(lblNewProject, "OpenProjectDialog.NewProject");
-			lblNewProject.Location = new System.Drawing.Point(0, 205);
-			lblNewProject.Margin = new System.Windows.Forms.Padding(0, 20, 0, 10);
-			lblNewProject.Name = "lblNewProject";
-			lblNewProject.Size = new System.Drawing.Size(229, 18);
-			lblNewProject.TabIndex = 2;
-			lblNewProject.Text = "Create new project based on:";
 			// 
 			// m_l10NSharpExtender
 			// 
@@ -86,14 +68,14 @@
 			this.m_linkTextReleaseBundle.LinkColor = System.Drawing.Color.White;
 			this.m_l10NSharpExtender.SetLocalizableToolTip(this.m_linkTextReleaseBundle, null);
 			this.m_l10NSharpExtender.SetLocalizationComment(this.m_linkTextReleaseBundle, null);
-			this.m_l10NSharpExtender.SetLocalizingId(this.m_linkTextReleaseBundle, "DialogBoxes.OpenProjectDlg.m_linkTextReleaseBundle");
-			this.m_linkTextReleaseBundle.Location = new System.Drawing.Point(0, 233);
+			this.m_l10NSharpExtender.SetLocalizingId(this.m_linkTextReleaseBundle, "DialogBoxes.OpenProjectDlg.CreateNewProject");
+			this.m_linkTextReleaseBundle.Location = new System.Drawing.Point(0, 255);
 			this.m_linkTextReleaseBundle.Margin = new System.Windows.Forms.Padding(0, 0, 0, 4);
 			this.m_linkTextReleaseBundle.Name = "m_linkTextReleaseBundle";
-			this.m_linkTextReleaseBundle.Size = new System.Drawing.Size(143, 18);
+			this.m_linkTextReleaseBundle.Size = new System.Drawing.Size(293, 18);
 			this.m_linkTextReleaseBundle.TabIndex = 3;
 			this.m_linkTextReleaseBundle.TabStop = true;
-			this.m_linkTextReleaseBundle.Text = "Text Release Bundle";
+			this.m_linkTextReleaseBundle.Text = "Create new project from text release bundle";
 			this.m_linkTextReleaseBundle.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.m_linkTextReleaseBundle_LinkClicked);
 			// 
 			// m_btnOk
@@ -129,22 +111,6 @@
 			this.m_btnCancel.Text = "Cancel";
 			this.m_btnCancel.UseVisualStyleBackColor = false;
 			// 
-			// m_linkParatextProject
-			// 
-			this.m_linkParatextProject.AutoSize = true;
-			this.m_linkParatextProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.m_linkParatextProject.LinkColor = System.Drawing.Color.White;
-			this.m_l10NSharpExtender.SetLocalizableToolTip(this.m_linkParatextProject, null);
-			this.m_l10NSharpExtender.SetLocalizationComment(this.m_linkParatextProject, null);
-			this.m_l10NSharpExtender.SetLocalizingId(this.m_linkParatextProject, "DialogBoxes.OpenProjectDlg.m_linkParatextProject");
-			this.m_linkParatextProject.Location = new System.Drawing.Point(0, 255);
-			this.m_linkParatextProject.Margin = new System.Windows.Forms.Padding(0, 0, 0, 4);
-			this.m_linkParatextProject.Name = "m_linkParatextProject";
-			this.m_linkParatextProject.Size = new System.Drawing.Size(111, 18);
-			this.m_linkParatextProject.TabIndex = 4;
-			this.m_linkParatextProject.TabStop = true;
-			this.m_linkParatextProject.Text = "Paratext project";
-			// 
 			// m_chkShowInactiveProjects
 			// 
 			this.m_chkShowInactiveProjects.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -153,7 +119,7 @@
 			this.m_l10NSharpExtender.SetLocalizableToolTip(this.m_chkShowInactiveProjects, null);
 			this.m_l10NSharpExtender.SetLocalizationComment(this.m_chkShowInactiveProjects, null);
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_chkShowInactiveProjects, "DialogBoxes.OpenProjectDialog.OpenProjectDlg.m_chkShowInactiveProjects");
-			this.m_chkShowInactiveProjects.Location = new System.Drawing.Point(345, 165);
+			this.m_chkShowInactiveProjects.Location = new System.Drawing.Point(345, 235);
 			this.m_chkShowInactiveProjects.Name = "m_chkShowInactiveProjects";
 			this.m_chkShowInactiveProjects.Size = new System.Drawing.Size(133, 17);
 			this.m_chkShowInactiveProjects.TabIndex = 6;
@@ -161,32 +127,6 @@
 			this.m_chkShowInactiveProjects.UseVisualStyleBackColor = true;
 			this.m_chkShowInactiveProjects.Visible = false;
 			this.m_chkShowInactiveProjects.CheckedChanged += new System.EventHandler(this.HandleShowHiddenProjectsCheckedChanged);
-			// 
-			// m_tableLayoutPanelMain
-			// 
-			this.m_tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.m_tableLayoutPanelMain.ColumnCount = 1;
-			this.m_tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.m_tableLayoutPanelMain.Controls.Add(this.lblExistingProject, 0, 0);
-			this.m_tableLayoutPanelMain.Controls.Add(this.m_linkParatextProject, 0, 5);
-			this.m_tableLayoutPanelMain.Controls.Add(lblNewProject, 0, 3);
-			this.m_tableLayoutPanelMain.Controls.Add(this.m_linkTextReleaseBundle, 0, 4);
-			this.m_tableLayoutPanelMain.Controls.Add(this.m_listExistingProjects, 0, 1);
-			this.m_tableLayoutPanelMain.Controls.Add(this.m_chkShowInactiveProjects, 0, 2);
-			this.m_tableLayoutPanelMain.Location = new System.Drawing.Point(15, 15);
-			this.m_tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(0);
-			this.m_tableLayoutPanelMain.Name = "m_tableLayoutPanelMain";
-			this.m_tableLayoutPanelMain.RowCount = 6;
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.m_tableLayoutPanelMain.Size = new System.Drawing.Size(481, 277);
-			this.m_tableLayoutPanelMain.TabIndex = 0;
 			// 
 			// m_listExistingProjects
 			// 
@@ -199,11 +139,35 @@
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_listExistingProjects, "DialogBoxes.OpenProjectDialog.ExistingProjectsList");
 			this.m_listExistingProjects.Location = new System.Drawing.Point(3, 25);
 			this.m_listExistingProjects.Name = "m_listExistingProjects";
-			this.m_listExistingProjects.Size = new System.Drawing.Size(475, 134);
+			this.m_listExistingProjects.Size = new System.Drawing.Size(475, 204);
 			this.m_listExistingProjects.TabIndex = 5;
 			this.m_listExistingProjects.SelectedProjectChanged += new System.EventHandler(this.HandleSelectedProjectChanged);
 			this.m_listExistingProjects.ListLoaded += new System.EventHandler(this.HandleExistingProjectsListLoaded);
 			this.m_listExistingProjects.DoubleClick += new System.EventHandler(this.HandleExistingProjectsDoubleClick);
+			// 
+			// m_tableLayoutPanelMain
+			// 
+			this.m_tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_tableLayoutPanelMain.ColumnCount = 1;
+			this.m_tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.m_tableLayoutPanelMain.Controls.Add(this.lblExistingProject, 0, 0);
+			this.m_tableLayoutPanelMain.Controls.Add(this.m_linkTextReleaseBundle, 0, 3);
+			this.m_tableLayoutPanelMain.Controls.Add(this.m_listExistingProjects, 0, 1);
+			this.m_tableLayoutPanelMain.Controls.Add(this.m_chkShowInactiveProjects, 0, 2);
+			this.m_tableLayoutPanelMain.Location = new System.Drawing.Point(15, 15);
+			this.m_tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(0);
+			this.m_tableLayoutPanelMain.Name = "m_tableLayoutPanelMain";
+			this.m_tableLayoutPanelMain.RowCount = 4;
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.m_tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.m_tableLayoutPanelMain.Size = new System.Drawing.Size(481, 277);
+			this.m_tableLayoutPanelMain.TabIndex = 0;
 			// 
 			// OpenProjectDlg
 			// 
@@ -239,7 +203,6 @@
 
 		private L10NSharp.UI.L10NSharpExtender m_l10NSharpExtender;
 		private System.Windows.Forms.TableLayoutPanel m_tableLayoutPanelMain;
-		private System.Windows.Forms.LinkLabel m_linkParatextProject;
 		private System.Windows.Forms.Label lblExistingProject;
 		private System.Windows.Forms.LinkLabel m_linkTextReleaseBundle;
 		private System.Windows.Forms.Button m_btnOk;
