adc0c8668a613515e645217e0081bb518a85ea93
Tom Bogle <tom_bogle@sil.org>
2015-05-18 10:08:43 -0400

    Fixes to make first-time (non-upgrade) installation work.

--- Glyssen/ProtoscriptGeneratorData.cs
+++ Glyssen/ProtoscriptGeneratorData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Xml.Serialization;
+using Glyssen.Properties;
 using SIL.Xml;
 
 namespace Glyssen
@@ -17,10 +18,24 @@ namespace Glyssen
 		{
 			exception = null;
 
+			ApplicationMetadata metadata;
 			if (!File.Exists(FilePath))
-				return new ApplicationMetadata();
-
-			var metadata = XmlSerializationHelper.DeserializeFromFile<ApplicationMetadata>(FilePath, out exception);
+			{
+				metadata = new ApplicationMetadata();
+				if (!Directory.Exists(Program.BaseDataFolder))
+				{
+					// In production, Installer is responsible for creating the base data folder.
+					// The version number will be initially set to 0, but since their won't be any
+					// projects to migrate, the migrator won't do anything but set the version number.
+					// However, on a developer machine (or in the event that a user has blown away or
+					// renamed the folder), we need to force its creation now.
+					Directory.CreateDirectory(Program.BaseDataFolder);
+					metadata.DataVersion = Settings.Default.DataFormatVersion;
+					metadata.Save();
+				}
+			}
+			else
+				metadata = XmlSerializationHelper.DeserializeFromFile<ApplicationMetadata>(FilePath, out exception);
 			return metadata;
 		}
 
