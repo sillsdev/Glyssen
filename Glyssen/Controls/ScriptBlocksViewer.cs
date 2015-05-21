9257bba83530a95998f86bdc5426a5e8616227ba
Andrew Polk <andrew_polk@sil.org>
2015-05-21 14:55:53 -0400

    Fix inefficiency when switching view types

--- Glyssen/Controls/ScriptBlocksViewer.cs
+++ Glyssen/Controls/ScriptBlocksViewer.cs
@@ -134,8 +134,8 @@ namespace Glyssen.Controls
 		#region private methods
 		private void ChangeViewType(Control viewBeingShown, Control viewBeingHidden)
 		{
-			viewBeingShown.Visible = true;
 			viewBeingHidden.Visible = false;
+			viewBeingShown.Visible = true;
 		}
 
 		private void UpdateContextBlocksDisplay(object sender, EventArgs args)
@@ -144,7 +144,7 @@ namespace Glyssen.Controls
 			{
 				if (m_blocksDisplayBrowser.Visible)
 					m_blocksDisplayBrowser.DisplayHtml(m_viewModel.Html);
-				else
+				else if (m_dataGridViewBlocks.Visible)
 					m_dataGridViewBlocks.UpdateContext();
 			}, true);
 		}
