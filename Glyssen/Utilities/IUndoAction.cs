489393e13b856960895d318dd884d970ac491488
Tom Bogle <tom_bogle@sil.org>
2015-09-28 10:51:22 -0400

    Added (unimplemented) class/interface to support undo

---
+++ Glyssen/Utilities/IUndoAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Glyssen
+{
+	public interface IUndoAction
+	{
+		/// <summary>
+		/// Reverses (or "undoes") an action.
+		///</summary>
+		bool Undo();
+
+		/// <summary> Reapplies (or "redoes") an action. </summary>
+		bool Redo();
+	}
+}
---
+++ Glyssen/Utilities/UndoStack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Glyssen
+{
+	/// <summary>
+	/// Collection of undoable/redoable IUndoActions with a "pointer" to the current position, so that if a new action
+	/// is pushed when the current pointer is not at the top of the stack, any items following that postion are pruned
+	/// from the collection.
+	/// </summary>
+	public class UndoStack
+	{
+		private List<IUndoAction> m_actions = new List<IUndoAction>();
+		private int m_current = 0;
+
+		public bool CanUndo { get { return true; } }
+		public bool CanRedo { get { return true; } }
+
+		public bool Undo()
+		{
+			return true;
+		}
+
+		public bool Redo()
+		{
+			return true;
+		}
+
+		public void Push()
+		{
+
+		}
+	}
+}
