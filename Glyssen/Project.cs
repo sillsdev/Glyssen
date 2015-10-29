c89194e0e05110fcf63135e2ba1add2519f6879a
Andrew Polk <andrew_polk@sil.org>
2015-10-29 16:40:01 -0400

    Provide all necessary warnings when user is exporting a script (PG-453)

--- Glyssen/Project.cs
+++ Glyssen/Project.cs
@@ -405,6 +405,11 @@ namespace Glyssen
 			get { return m_characterGroupList ?? (m_characterGroupList = LoadCharacterGroupData()); }
 		}
 
+		public bool IsVoiceActorScriptReady
+		{
+			get { return IsVoiceActorAssignmentsComplete && EveryAssignedGroupHasACharacter && !HasUnusedActor; }
+		}
+
 		public bool IsVoiceActorAssignmentsComplete
 		{
 			get
@@ -412,6 +417,16 @@ namespace Glyssen
 				var groups = CharacterGroupList.CharacterGroups;
 				return groups.Count > 0 && groups.All(t => t.IsVoiceActorAssigned);
 			}
+		}
+
+		public bool EveryAssignedGroupHasACharacter
+		{
+			get { return CharacterGroupList.AssignedGroups.All(g => g.CharacterIds.Count != 0); }
+		}
+
+		public bool HasUnusedActor
+		{
+			get { return VoiceActorList.Actors.Any(actor => !CharacterGroupList.HasVoiceActorAssigned(actor.Id)); }
 		}
 
 		internal void ClearAssignCharacterStatus()
---
+++ GlyssenTests/ProjectTests.cs
@@ -0,0 +1,98 @@
+﻿using Glyssen.Character;
+using NUnit.Framework;
+
+namespace GlyssenTests
+{
+	internal class ProjectTests
+	{
+		[Test]
+		public void IsVoiceActorAssignmentsComplete_Complete_ReturnsTrue()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor = new Glyssen.VoiceActor.VoiceActor();
+			project.VoiceActorList.Actors.Add(actor);
+			var group = new CharacterGroup(project);
+			group.AssignVoiceActor(actor.Id);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.True(project.IsVoiceActorAssignmentsComplete);
+		}
+
+		[Test]
+		public void IsVoiceActorAssignmentsComplete_GroupWithNoActor_ReturnsFalse()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor = new Glyssen.VoiceActor.VoiceActor();
+			project.VoiceActorList.Actors.Add(actor);
+			var group = new CharacterGroup(project);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.False(project.IsVoiceActorAssignmentsComplete);
+		}
+
+		[Test]
+		public void EveryAssignedGroupHasACharacter_EveryAssignedGroupHasACharacter_ReturnsTrue()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor = new Glyssen.VoiceActor.VoiceActor();
+			project.VoiceActorList.Actors.Add(actor);
+			var group = new CharacterGroup(project);
+			group.CharacterIds.Add("Bob");
+			group.AssignVoiceActor(actor.Id);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.True(project.CharacterGroupList.AnyVoiceActorAssigned());
+			Assert.True(project.EveryAssignedGroupHasACharacter);
+		}
+
+		[Test]
+		public void EveryAssignedGroupHasACharacter_AssignedGroupWithNoCharacter_ReturnsFalse()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor = new Glyssen.VoiceActor.VoiceActor();
+			project.VoiceActorList.Actors.Add(actor);
+			var group = new CharacterGroup(project);
+			group.AssignVoiceActor(actor.Id);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.True(project.CharacterGroupList.AnyVoiceActorAssigned());
+			Assert.False(project.EveryAssignedGroupHasACharacter);
+		}
+
+		[Test]
+		public void HasUnusedActor_NoUnusedActor_ReturnsFalse()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor1 = new Glyssen.VoiceActor.VoiceActor();
+			project.VoiceActorList.Actors.Add(actor1);
+			var group = new CharacterGroup(project);
+			group.AssignVoiceActor(actor1.Id);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.True(project.CharacterGroupList.AnyVoiceActorAssigned());
+			Assert.False(project.HasUnusedActor);
+		}
+
+		[Test]
+		public void HasUnusedActor_UnusedActor_ReturnsTrue()
+		{
+			var project = TestProject.CreateBasicTestProject();
+
+			var actor1 = new Glyssen.VoiceActor.VoiceActor { Id = 0 };
+			project.VoiceActorList.Actors.Add(actor1);
+			var actor2 = new Glyssen.VoiceActor.VoiceActor { Id = 1 };
+			project.VoiceActorList.Actors.Add(actor2);
+			var group = new CharacterGroup(project);
+			group.AssignVoiceActor(actor1.Id);
+			project.CharacterGroupList.CharacterGroups.Add(group);
+
+			Assert.True(project.CharacterGroupList.AnyVoiceActorAssigned());
+			Assert.True(project.HasUnusedActor);
+		}
+	}
+}
