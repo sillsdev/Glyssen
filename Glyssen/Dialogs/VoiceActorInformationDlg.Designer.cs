c2aa08738edb4b336ee61ecee5d0ebcba36e427c
Andrew Polk <andrew_polk@sil.org>
2015-12-16 10:21:39 -0500

    Add inoperable Help buttons to phase 2 screens per Bryan's request

--- Glyssen/Dialogs/VoiceActorInformationDlg.Designer.cs
+++ Glyssen/Dialogs/VoiceActorInformationDlg.Designer.cs
@@ -44,10 +44,13 @@ namespace Glyssen.Dialogs
 			this.m_linkNarrationPreferences = new System.Windows.Forms.LinkLabel();
 			this.m_linkMoreInfo = new System.Windows.Forms.LinkLabel();
 			this.m_lblTally = new System.Windows.Forms.Label();
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.m_dataGrid = new Glyssen.Controls.VoiceActorInformationGrid();
 			this.m_saveStatus = new Glyssen.Controls.SaveStatus();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.m_toolStripButtonHelp = new System.Windows.Forms.ToolStripButton();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			((System.ComponentModel.ISupportInitialize)(this.l10NSharpExtender1)).BeginInit();
+			this.toolStrip1.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -73,7 +76,7 @@ namespace Glyssen.Dialogs
 			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_btnNext, null);
 			this.l10NSharpExtender1.SetLocalizationComment(this.m_btnNext, null);
 			this.l10NSharpExtender1.SetLocalizingId(this.m_btnNext, "Common.Next");
-			this.m_btnNext.Location = new System.Drawing.Point(504, 312);
+			this.m_btnNext.Location = new System.Drawing.Point(504, 296);
 			this.m_btnNext.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
 			this.m_btnNext.Name = "m_btnNext";
 			this.m_btnNext.Size = new System.Drawing.Size(75, 23);
@@ -95,7 +98,7 @@ namespace Glyssen.Dialogs
 			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_linkClose, null);
 			this.l10NSharpExtender1.SetLocalizationComment(this.m_linkClose, null);
 			this.l10NSharpExtender1.SetLocalizingId(this.m_linkClose, "DialogBoxes.VoiceActorInformation.Close");
-			this.m_linkClose.Location = new System.Drawing.Point(546, 376);
+			this.m_linkClose.Location = new System.Drawing.Point(546, 360);
 			this.m_linkClose.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
 			this.m_linkClose.Name = "m_linkClose";
 			this.m_linkClose.Size = new System.Drawing.Size(33, 13);
@@ -111,7 +114,7 @@ namespace Glyssen.Dialogs
 			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_btnOk, null);
 			this.l10NSharpExtender1.SetLocalizationComment(this.m_btnOk, null);
 			this.l10NSharpExtender1.SetLocalizingId(this.m_btnOk, "Common.OK");
-			this.m_btnOk.Location = new System.Drawing.Point(504, 344);
+			this.m_btnOk.Location = new System.Drawing.Point(504, 328);
 			this.m_btnOk.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
 			this.m_btnOk.Name = "m_btnOk";
 			this.m_btnOk.Size = new System.Drawing.Size(75, 23);
@@ -210,12 +213,76 @@ namespace Glyssen.Dialogs
 			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_lblTally, null);
 			this.l10NSharpExtender1.SetLocalizationComment(this.m_lblTally, null);
 			this.l10NSharpExtender1.SetLocalizingId(this.m_lblTally, "DialogBoxes.VoiceActorInformation.Tally");
-			this.m_lblTally.Location = new System.Drawing.Point(3, 290);
+			this.m_lblTally.Location = new System.Drawing.Point(3, 274);
 			this.m_lblTally.Name = "m_lblTally";
 			this.m_lblTally.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
 			this.m_lblTally.Size = new System.Drawing.Size(178, 16);
 			this.m_lblTally.TabIndex = 12;
 			this.m_lblTally.Text = "Tally: {0} Male, {1} Female, {2} Child";
+			// 
+			// m_dataGrid
+			// 
+			this.m_dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_dataGrid.AutoScroll = true;
+			this.m_dataGrid.AutoSize = true;
+			this.m_dataGrid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
+			this.m_dataGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
+			this.tableLayoutPanel1.SetColumnSpan(this.m_dataGrid, 4);
+			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_dataGrid, null);
+			this.l10NSharpExtender1.SetLocalizationComment(this.m_dataGrid, null);
+			this.l10NSharpExtender1.SetLocalizingId(this.m_dataGrid, "DialogBoxes.VoiceActorInformation.voiceActorInformationGrid21");
+			this.m_dataGrid.Location = new System.Drawing.Point(0, 111);
+			this.m_dataGrid.Margin = new System.Windows.Forms.Padding(0);
+			this.m_dataGrid.Name = "m_dataGrid";
+			this.m_dataGrid.ReadOnly = false;
+			this.m_dataGrid.Size = new System.Drawing.Size(582, 163);
+			this.m_dataGrid.TabIndex = 3;
+			// 
+			// m_saveStatus
+			// 
+			this.m_saveStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_saveStatus.AutoSize = true;
+			this.m_saveStatus.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.m_saveStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
+			this.m_saveStatus.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
+			this.m_saveStatus.ForeColor = System.Drawing.Color.White;
+			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_saveStatus, null);
+			this.l10NSharpExtender1.SetLocalizationComment(this.m_saveStatus, null);
+			this.l10NSharpExtender1.SetLocalizingId(this.m_saveStatus, "DialogBoxes.VoiceActorInformation.SaveStatus");
+			this.m_saveStatus.Location = new System.Drawing.Point(485, 76);
+			this.m_saveStatus.Margin = new System.Windows.Forms.Padding(0, 0, 0, 6);
+			this.m_saveStatus.Name = "m_saveStatus";
+			this.m_saveStatus.Size = new System.Drawing.Size(97, 13);
+			this.m_saveStatus.TabIndex = 4;
+			this.m_saveStatus.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_toolStripButtonHelp});
+			this.l10NSharpExtender1.SetLocalizableToolTip(this.toolStrip1, null);
+			this.l10NSharpExtender1.SetLocalizationComment(this.toolStrip1, null);
+			this.l10NSharpExtender1.SetLocalizingId(this.toolStrip1, "DialogBoxes.VoiceActorInformation.toolStrip1");
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(609, 25);
+			this.toolStrip1.TabIndex = 7;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// m_toolStripButtonHelp
+			// 
+			this.m_toolStripButtonHelp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.m_toolStripButtonHelp.Image = global::Glyssen.Properties.Resources.helpSmall;
+			this.m_toolStripButtonHelp.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_toolStripButtonHelp, null);
+			this.l10NSharpExtender1.SetLocalizationComment(this.m_toolStripButtonHelp, null);
+			this.l10NSharpExtender1.SetLocalizingId(this.m_toolStripButtonHelp, "Common.Help");
+			this.m_toolStripButtonHelp.Name = "m_toolStripButtonHelp";
+			this.m_toolStripButtonHelp.Size = new System.Drawing.Size(52, 22);
+			this.m_toolStripButtonHelp.Text = "Help";
 			// 
 			// tableLayoutPanel1
 			// 
@@ -239,7 +306,7 @@ namespace Glyssen.Dialogs
 			this.tableLayoutPanel1.Controls.Add(this.m_lblInstructions, 0, 5);
 			this.tableLayoutPanel1.Controls.Add(this.m_saveStatus, 3, 5);
 			this.tableLayoutPanel1.Controls.Add(this.m_lblTally, 0, 8);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 12);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 31);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 12;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
@@ -254,46 +321,8 @@ namespace Glyssen.Dialogs
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(582, 389);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(582, 373);
 			this.tableLayoutPanel1.TabIndex = 6;
-			// 
-			// m_dataGrid
-			// 
-			this.m_dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.m_dataGrid.AutoScroll = true;
-			this.m_dataGrid.AutoSize = true;
-			this.m_dataGrid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
-			this.m_dataGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
-			this.tableLayoutPanel1.SetColumnSpan(this.m_dataGrid, 4);
-			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_dataGrid, null);
-			this.l10NSharpExtender1.SetLocalizationComment(this.m_dataGrid, null);
-			this.l10NSharpExtender1.SetLocalizingId(this.m_dataGrid, "DialogBoxes.VoiceActorInformation.voiceActorInformationGrid21");
-			this.m_dataGrid.Location = new System.Drawing.Point(0, 111);
-			this.m_dataGrid.Margin = new System.Windows.Forms.Padding(0);
-			this.m_dataGrid.Name = "m_dataGrid";
-			this.m_dataGrid.ReadOnly = false;
-			this.m_dataGrid.Size = new System.Drawing.Size(582, 179);
-			this.m_dataGrid.TabIndex = 3;
-			// 
-			// m_saveStatus
-			// 
-			this.m_saveStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.m_saveStatus.AutoSize = true;
-			this.m_saveStatus.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.m_saveStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
-			this.m_saveStatus.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
-			this.m_saveStatus.ForeColor = System.Drawing.Color.White;
-			this.l10NSharpExtender1.SetLocalizableToolTip(this.m_saveStatus, null);
-			this.l10NSharpExtender1.SetLocalizationComment(this.m_saveStatus, null);
-			this.l10NSharpExtender1.SetLocalizingId(this.m_saveStatus, "DialogBoxes.VoiceActorInformation.SaveStatus");
-			this.m_saveStatus.Location = new System.Drawing.Point(485, 76);
-			this.m_saveStatus.Margin = new System.Windows.Forms.Padding(0, 0, 0, 6);
-			this.m_saveStatus.Name = "m_saveStatus";
-			this.m_saveStatus.Size = new System.Drawing.Size(97, 13);
-			this.m_saveStatus.TabIndex = 4;
-			this.m_saveStatus.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// VoiceActorInformationDlg
 			// 
@@ -301,6 +330,7 @@ namespace Glyssen.Dialogs
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(73)))), ((int)(((byte)(108)))));
 			this.ClientSize = new System.Drawing.Size(609, 413);
+			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.l10NSharpExtender1.SetLocalizableToolTip(this, null);
@@ -314,9 +344,12 @@ namespace Glyssen.Dialogs
 			this.Text = "Voice Actor Information";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VoiceActorInformationDlg_FormClosing);
 			((System.ComponentModel.ISupportInitialize)(this.l10NSharpExtender1)).EndInit();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -336,5 +369,7 @@ namespace Glyssen.Dialogs
 		private System.Windows.Forms.LinkLabel m_linkNarrationPreferences;
 		private System.Windows.Forms.LinkLabel m_linkMoreInfo;
 		private System.Windows.Forms.Label m_lblTally;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton m_toolStripButtonHelp;
 	}
 }
