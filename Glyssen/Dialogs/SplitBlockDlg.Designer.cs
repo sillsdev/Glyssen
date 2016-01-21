f4adac422e8e29637463cd50bb75830103c10eb0
Andrew Polk <andrew_polk@sil.org>
2016-01-21 10:55:12 -0500

    Increase size of Split Block dialog and remember user resize

--- Glyssen/Dialogs/SplitBlockDlg.Designer.cs
+++ Glyssen/Dialogs/SplitBlockDlg.Designer.cs
@@ -63,7 +63,7 @@
 			this.m_l10NSharpExtender.SetLocalizationComment(this.m_btnCancel, null);
 			this.m_l10NSharpExtender.SetLocalizationPriority(this.m_btnCancel, L10NSharp.LocalizationPriority.High);
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_btnCancel, "Common.Cancel");
-			this.m_btnCancel.Location = new System.Drawing.Point(351, 227);
+			this.m_btnCancel.Location = new System.Drawing.Point(442, 321);
 			this.m_btnCancel.Name = "m_btnCancel";
 			this.m_btnCancel.Size = new System.Drawing.Size(75, 23);
 			this.m_btnCancel.TabIndex = 3;
@@ -79,7 +79,7 @@
 			this.m_l10NSharpExtender.SetLocalizationComment(this.m_btnOk, null);
 			this.m_l10NSharpExtender.SetLocalizationPriority(this.m_btnOk, L10NSharp.LocalizationPriority.High);
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_btnOk, "Common.OK");
-			this.m_btnOk.Location = new System.Drawing.Point(270, 227);
+			this.m_btnOk.Location = new System.Drawing.Point(361, 321);
 			this.m_btnOk.Name = "m_btnOk";
 			this.m_btnOk.Size = new System.Drawing.Size(75, 23);
 			this.m_btnOk.TabIndex = 4;
@@ -94,7 +94,7 @@
 			this.m_l10NSharpExtender.SetLocalizableToolTip(this.m_lblInvalidSplitLocation, null);
 			this.m_l10NSharpExtender.SetLocalizationComment(this.m_lblInvalidSplitLocation, null);
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_lblInvalidSplitLocation, "DialogBoxes.SplitBlockDlg.InvalidSplitLocation");
-			this.m_lblInvalidSplitLocation.Location = new System.Drawing.Point(13, 215);
+			this.m_lblInvalidSplitLocation.Location = new System.Drawing.Point(13, 309);
 			this.m_lblInvalidSplitLocation.Name = "m_lblInvalidSplitLocation";
 			this.m_lblInvalidSplitLocation.Size = new System.Drawing.Size(165, 13);
 			this.m_lblInvalidSplitLocation.TabIndex = 5;
@@ -113,7 +113,7 @@
 			this.m_l10NSharpExtender.SetLocalizingId(this.m_blocksDisplayBrowser, "DialogBoxes.SplitBlockDlg.Browser");
 			this.m_blocksDisplayBrowser.Location = new System.Drawing.Point(12, 39);
 			this.m_blocksDisplayBrowser.Name = "m_blocksDisplayBrowser";
-			this.m_blocksDisplayBrowser.Size = new System.Drawing.Size(414, 169);
+			this.m_blocksDisplayBrowser.Size = new System.Drawing.Size(505, 263);
 			this.m_blocksDisplayBrowser.TabIndex = 1;
 			this.m_blocksDisplayBrowser.OnMouseClick += new System.EventHandler<Gecko.DomMouseEventArgs>(this.InsertSplitLocation);
 			// 
@@ -123,12 +123,13 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.m_btnCancel;
-			this.ClientSize = new System.Drawing.Size(438, 262);
+			this.ClientSize = new System.Drawing.Size(529, 356);
 			this.Controls.Add(this.m_lblInvalidSplitLocation);
 			this.Controls.Add(this.m_btnOk);
 			this.Controls.Add(this.m_btnCancel);
 			this.Controls.Add(this.m_lblInstructions);
 			this.Controls.Add(this.m_blocksDisplayBrowser);
+			this.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_l10NSharpExtender.SetLocalizableToolTip(this, null);
 			this.m_l10NSharpExtender.SetLocalizationComment(this, null);
 			this.m_l10NSharpExtender.SetLocalizingId(this, "DialogBoxes.SplitBlockDlg.WindowTitle");
