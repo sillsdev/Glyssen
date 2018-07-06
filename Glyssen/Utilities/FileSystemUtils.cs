ad72f20fef6ba667c535ea1f143da9d7a5bb61e2
andrew-polk <andrew_polk@sil.org>
2018-07-06 16:15:56 -0700

    Clean up FileSystemUtils

--- Glyssen/Utilities/FileSystemUtils.cs
+++ Glyssen/Utilities/FileSystemUtils.cs
@@ -1,29 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Glyssen.Utilities
 {
 	public static class FileSystemUtils
 	{
-		public static string GetShortName(string path)
+		public static void SafeCreateAndOpenFolder(string folderPath)
 		{
-			var shortBuilder = new StringBuilder(300);
-			GetShortPathName(path, shortBuilder, (uint)shortBuilder.Capacity);
-			return shortBuilder.ToString();
-		}
-
-		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern uint GetShortPathName(
-		   [MarshalAs(UnmanagedType.LPTStr)]string lpszLongPath,
-		   [MarshalAs(UnmanagedType.LPTStr)]StringBuilder lpszShortPath,
-		   uint cchBuffer);
-
-		public static void SafeCreateAndOpenFolder(string folderPath, bool replaceNonBreakSpacesWithNormalSpaces = true)
-		{
-			if (replaceNonBreakSpacesWithNormalSpaces)
 			try
 			{
 				Directory.CreateDirectory(folderPath);
