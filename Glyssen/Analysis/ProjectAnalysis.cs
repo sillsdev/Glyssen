65282cee13520d036107a44617f53318189cbd6e
tom_bogle@sil.org <tom_bogle@sil.org>
2019-08-27 11:16:53 -0400

    Added message boxes to alert the user to the presence of blocks that are marked as "Needs Review" before assigning voice actors or exporting the script.

--- Glyssen/Analysis/ProjectAnalysis.cs
+++ Glyssen/Analysis/ProjectAnalysis.cs
@@ -20,6 +20,7 @@ namespace Glyssen.Analysis
 		public int NarratorBlocks { get; private set; }
 		public int UnknownBlocks { get; private set; }
 		public int AmbiguousBlocks { get; private set; }
+		public int NeedsReviewBlocks { get; private set; }
 		public double TotalPercentAssigned { get; private set; }
 		public int UserAssignedBlocks { get; private set; }
 		public int NeedsAssignment { get; private set; }
@@ -41,6 +42,7 @@ namespace Glyssen.Analysis
 			NarratorBlocks = 0;
 			UnknownBlocks = 0;
 			AmbiguousBlocks = 0;
+			NeedsReviewBlocks = 0;
 			UserAssignedBlocks = 0;
 			NeedsAssignment = 0;
 			foreach (BookScript book in m_projectToAnalyze.IncludedBooks)
@@ -63,6 +65,8 @@ namespace Glyssen.Analysis
 						UnknownBlocks++;
 					else if (block.CharacterId == CharacterVerseData.kAmbiguousCharacter)
 						AmbiguousBlocks++;
+					else if (block.CharacterId == CharacterVerseData.kNeedsReview)
+						NeedsReviewBlocks++;
 					if (block.UserConfirmed)
 						UserAssignedBlocks++;
 					if (block.UserConfirmed || block.CharacterIsUnclear)
@@ -89,6 +93,8 @@ namespace Glyssen.Analysis
 				m_alignmentPercent = 0;
 				return;
 			}
+			// Note: Since we want this to be as efficient as possible and all we care about here is statistics,
+			// we don't need to bother with applying narrator overrides.
 			foreach (var book in refText.GetBooksWithBlocksConnectedToReferenceText(m_projectToAnalyze, false))
 			{
 				var blocks = book.GetScriptBlocks();
