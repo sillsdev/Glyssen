59088b91b8dc3ef458e5c423e2592706248bbf0f
tom_bogle@sil.org <tom_bogle@sil.org>
2019-09-30 15:25:59 -0400

    Removed trailing space in comment

--- Glyssen/BookBlockIndices.cs
+++ Glyssen/BookBlockIndices.cs
@@ -44,7 +44,7 @@ namespace Glyssen
 
 		/// <summary>
 		/// This will always return a value which is >= 1. This is needed when the actual
-		/// number of blocks is needed, because unfortunately MultiBlockCount can have a 
+		/// number of blocks is needed, because unfortunately MultiBlockCount can have a
 		/// value or either 1 or 0, and either way, it represents a single block.
 		/// </summary>
 		public uint BlockCount => MultiBlockCount > 1 ? MultiBlockCount : 1;
