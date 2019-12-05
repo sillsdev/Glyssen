28298de56235f7d08a1174bdab036dfa7f82248b
tom_bogle@sil.org <tom_bogle@sil.org>
2019-12-05 11:06:37 -0500

    Moved RelatedCharactersTests under GlyssenEngineTests.
    (Note: the above change required GlyssenTests to reference GlyssenEngineTests and have TestRelatedCharacters resource be public because some classes are not yet moved from GlyssenTests to GlyssenEngineTests.)
    Added CharacterVerseDataTests.
    Renamed existing CharacterVerseDataTests to ControlCharacterVerseDataTests.
    Added ControlDataIntegrityTest to ensure that extra-biblical character IDs (however we define them) are not used in CV control file.

---
+++ GlyssenEngineTests/Character/CharacterVerseDataTests.cs
@@ -0,0 +1,49 @@
+﻿using GlyssenEngine.Character;
+using NUnit.Framework;
+
+namespace GlyssenEngineTests.Character
+{
+	/// <summary>
+	/// Note that these tests purport to test the GetCharacters method, but in fact that is just a simple LINQ statement;
+	/// they're really testing the Load method.
+	/// </summary>
+	[TestFixture]
+	class CharacterVerseDataTests
+	{
+		[TestCase("BC-GEN")]
+		[TestCase("extra-MAT")]
+		[TestCase("intro-REV")]
+		public void IsCharacterExtraBiblical_ExtraBiblicalCharacter_ReturnsTrue(string characterId)
+		{
+			Assert.IsTrue(CharacterVerseData.IsCharacterExtraBiblical(characterId));
+		}
+
+		[TestCase("BCGEN")]
+		// ENHANCE: Character ID should only be considered extra-biblical if prefix is followed by book ID. [TestCase("extra-weird guy")]
+		[TestCase("-extra-")]
+		[TestCase("introduction of the new king")]
+		[TestCase("Joseph")]
+		public void IsCharacterExtraBiblical_NormalCharacter_ReturnsFalse(string characterId)
+		{
+			Assert.IsFalse(CharacterVerseData.IsCharacterExtraBiblical(characterId));
+		}
+
+		[TestCase("narrator-MAL")]
+		[TestCase("BC-GEN")]
+		[TestCase("extra-MAT")]
+		[TestCase("intro-REV")]
+		public void IsCharacterStandard_StandardCharacter_ReturnsTrue(string characterId)
+		{
+			Assert.IsTrue(CharacterVerseData.IsCharacterStandard(characterId));
+		}
+
+		[TestCase("Jesus")]
+		[TestCase("narrator's friend")]
+		[TestCase("extraordinary dude in MAT")]
+		[TestCase("someone speaking in intro-REV")]
+		public void IsCharacterStandard_NormalCharacter_ReturnsTrue(string characterId)
+		{
+			Assert.IsFalse(CharacterVerseData.IsCharacterStandard(characterId));
+		}
+	}
+}
