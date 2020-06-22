228f828030da7a66e8d5f85e02ad50c66d1c0776
Andrew Christensen <achristensen@fcbhmail.org>
2020-06-22 13:17:10 -0400

    GlyssenEngine in Vessel - Proof of concept

---
+++ VesselTest/TestUsingGlyssen.cs
@@ -0,0 +1,26 @@
+﻿using GlyssenEngine.Bundle;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VesselTest
+{
+	public class TestUsingGlyssen
+	{
+		[Test]
+		public void TestCreateGlyssenConnection()
+		{
+			var bundleFilePath =
+				@"C:\Users\andyc\Documents\Faith Comes By Hearing\Projects\Vessel\IntegrationTests\Services\Test Files\TestBundleFile.zip";
+			bundleFilePath = @"C:\Temp\TestBundleFile.zip";
+
+			//Act
+			var bundle = new GlyssenBundle(bundleFilePath);
+			//var xxx = 1;
+			Assert.AreEqual(1, 1);
+		}
+	}
+}
