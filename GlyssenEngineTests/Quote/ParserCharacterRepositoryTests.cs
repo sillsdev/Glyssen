9371411079078cf66458bd275db6747f87b2b5b3
Andrew Christensen <achristensen@fcbhmail.org>
2020-08-17 18:35:26 -0400

    For Vessel: Ignore Hypothetical quotes types when reference text = null
    Ignore Hypothetical quotes types when the reference text is unavailable. This was done because Vessel does not assign a reference text.  Continued after Tom's simplified unit test

--- GlyssenEngineTests/Quote/ParserCharacterRepositoryTests.cs
+++ GlyssenEngineTests/Quote/ParserCharacterRepositoryTests.cs
@@ -9,6 +9,13 @@ namespace GlyssenEngineTests.Quote
 	[TestFixture]
 	public class ParserCharacterRepositoryTests
 	{
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			// Use a test version of the file so the tests won't break every time we fix a problem in the production control file.
+			ControlCharacterVerseData.TabDelimitedCharacterVerseData = Properties.Resources.TestCharacterVerseOct2015;
+		}
+
 		[TestCase(19, 10, "6", "man, wicked")]
 		[TestCase(40, 7, "8", "Jesus")]
 		public void ParserCharacterRepository_ReferenceTextIsNull_ReturnsSingleCharacterForVerse(int book, int chapter, string verse, string expectedCharacter)
@@ -25,11 +32,11 @@ namespace GlyssenEngineTests.Quote
 		{
 			var cvRepo = new ParserCharacterRepository(ControlCharacterVerseData.Singleton, null);
 
-			var characters = cvRepo.GetCharacters(45, 2, new Verse("17"));
+			var characters = cvRepo.GetCharacters(44, 8, new Verse("37"));
 
 			Assert.True(characters.Count == 2);
-			Assert.That(characters.Any(i => i.Character == "narrator-ROM"));
-			Assert.That(characters.Any(i => i.Character == "you (hypothetical)"));
+			Assert.That(characters.Any(i => i.Character == "Ethiopian officer of Queen Candace"));
+			Assert.That(characters.Any(i => i.Character == "Philip the evangelist"));
 		}
 	}
 }
