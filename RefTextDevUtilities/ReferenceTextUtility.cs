4da6c24da4bbcf14f7c4515421bda4a1034b216b
tombogle <tom_bogle@sil.org>
2021-05-26 21:20:46 -0400

    Changed to not treat it as an error when the number of blocks changes from one version of the DG to another

--- RefTextDevUtilities/ReferenceTextUtility.cs
+++ RefTextDevUtilities/ReferenceTextUtility.cs
@@ -490,10 +490,15 @@ namespace Glyssen.RefTextDevUtilities
 							{
 								existingRefBlocksForLanguage = existingReferenceTextForLanguage.Books
 									.SingleOrDefault(b => b.BookId == currBookId)?.GetScriptBlocks();
-								
+
 								if (existingRefBlocksForLanguage?.Count != existingEnglishRefBook?.GetScriptBlocks().Count)
+								{
+									// Only regard this as an "error" if the two reference texts claim to be on the same version number.
+									bool isError = currBookNum >= 40 ? s_existingEnglish.EnglishNTVersion == existingReferenceTextForLanguage.EnglishNTVersion :
+										s_existingEnglish.EnglishOTVersion == existingReferenceTextForLanguage.EnglishOTVersion;
 									WriteOutput($"Existing book of {currBookId} for {language} has {existingRefBlocksForLanguage?.Count} blocks, but the existing " +
-										$"English reference text has {existingEnglishRefBook?.GetScriptBlocks().Count} blocks.", true);
+										$"English reference text has {existingEnglishRefBook?.GetScriptBlocks().Count} blocks.", isError);
+								}
 							}
 
 							iBlockInExistingEnglishRefBook = 0;
