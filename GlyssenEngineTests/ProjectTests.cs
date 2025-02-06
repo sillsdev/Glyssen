4a4ca8fa0b68c52200d37d8e6f7f9d2cbdbc5169
tombogle <tom_bogle@sil.org>
2025-02-06 15:18:38 -0500

    Experiment: try changing order of Boaz and Mr. Rogers

--- GlyssenEngineTests/ProjectTests.cs
+++ GlyssenEngineTests/ProjectTests.cs
@@ -485,70 +485,10 @@ namespace GlyssenEngineTests
 			Assert.IsTrue(project.ReportingClauses.Contains("soup"));
 		}
 
+		[TestCase("Mr. Rogers")]
 		[TestCase("Boaz")]
 		public void UpdateProjectFromBundleData_ProjectHasCustomCharacterVerseDecisions_UserDecisionsReapplied(string character)
 		{
-			var testProject = new Project(BundleWithLdml);
-
-			TestContext.WriteLine($"Returned from Project constructor for {character}");
-
-			WaitForProjectInitializationToFinish(testProject, ProjectState.FullyInitialized);
-
-			var book = testProject.IncludedBooks.First();
-			var matchup = testProject.ReferenceText.GetBlocksForVerseMatchedToReferenceText(book,
-				book.GetScriptBlocks().Count - 1);
-
-			SetAndConfirmCharacterAndDeliveryForAllCorrelatedBlocks(matchup, book.BookNumber, testProject, character);
-
-			Assert.IsTrue(testProject.ProjectCharacterVerseData.Any());
-			if (!CharacterDetailData.Singleton.GetDictionary().ContainsKey(character))
-				testProject.AddProjectCharacterDetail(new CharacterDetail {CharacterId = character, Age = CharacterAge.Elder, Gender = CharacterGender.Male});
-
-			matchup.Apply();
-
-			bool complete = false;
-
-			var origCountOfUserConfirmedBlocks = book.GetScriptBlocks().Count(b => b.UserConfirmed);
-
-			var updatedProject = testProject.UpdateProjectFromBundleData(BundleWithLdml);
-
-			updatedProject.QuoteParseCompleted += delegate { complete = true; };
-
-			var timeoutCount = 0;
-			do
-			{
-				if (updatedProject.ProjectState == ProjectState.FullyInitialized)
-				{
-					if (!complete)
-						Console.WriteLine("Got to fully initialized state without firing QuoteParseCompleted");
-					break;
-				}
-				Thread.Sleep(100);
-				timeoutCount++;
-				if (timeoutCount > 100)
-					break;
-			} while (!complete);
-
-			if (!complete)
-				Assert.Fail("Update took too long. Maybe this is why it hangs frequently on TeamCity.");
-
-			var userConfirmedBlocksAfterReapplying = updatedProject.IncludedBooks.First().GetScriptBlocks().Where(b => b.UserConfirmed).ToList();
-			Assert.AreEqual(origCountOfUserConfirmedBlocks, userConfirmedBlocksAfterReapplying.Count);
-			foreach (var blockWithReappliedUserDecision in userConfirmedBlocksAfterReapplying)
-			{
-				Assert.AreEqual(character, blockWithReappliedUserDecision.CharacterId);
-				Assert.AreEqual("foamy", blockWithReappliedUserDecision.Delivery);
-			}
-
-			Assert.IsTrue(((PersistenceImplementation)Project.Writer).WasExpectedBackupCreated(
-				testProject, "Backup before updating from new bundle", true));
-		}
-
-		[Test]
-		public void UpdateProjectFromBundleData_ProjectHasMrRogersVerseDecisions_UserDecisionsReapplied()
-		{
-			string character = "Mr. Rogers";
-
 			var testProject = new Project(BundleWithLdml);
 
 			TestContext.WriteLine($"Returned from Project constructor for {character}");
