878d6ffa1ac778eb5b9518da0f1f8ebfac490b64
tombogle <tom_bogle@sil.org>
2025-05-22 15:17:58 -0400

    Updated SIL DLLs to v. 16.
    Updated most other dependencies to latest compatible minor or patch version
    Removed direct dependencies on DotNetZip (still needed in Glyssen itself because ParatextData requires it, but it gets trimmed if not explicitly referenced)
    Fixed problems with GlyssenShare generation, streamlined interfaces, and added error handling.

--- GlyssenTests/Utilities/GlyssenShareTests.cs
+++ GlyssenTests/Utilities/GlyssenShareTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Glyssen.Shared;
 using Glyssen.Utilities;
 using NUnit.Framework;
 using SIL.IO;
@@ -15,6 +12,13 @@ namespace GlyssenTests.Utilities
 	[TestFixture]
 	public class GlyssenShareTests
 	{
+		[Test]
+		public void Constructor_NullProject_ThrowsArgumentNullException()
+		{
+			Assert.That(() => new GlyssenShare((IUserProject)null),
+				Throws.InstanceOf<ArgumentNullException>().With.Property("ParamName").EqualTo("project"));
+		}
+
 		[Test]
 		public void Constructor_GlyssenshareFileInNonexistentFolder_ThrowsFileNotFoundException()
 		{
@@ -46,13 +50,70 @@ namespace GlyssenTests.Utilities
 			Assert.That(() => new GlyssenShare(filepath), Throws.InstanceOf<ArgumentException>());
 		}
 
-		[Test] public void Constructor_ValidGlyssenshareFile_Sets()
+		[Test] public void Constructor_InvalidGlyssenshareFileNoDirAndWrongNumberOfFolderLevels_ThrowsArgumentException()
+		{
+			using (var file = TempFile.FromResource(Properties.Resources.novaliddir, kShareFileExtension))
+			{
+				Assert.That(file.Path, Does.Exist, "SETUP - Sanity check");
+				Assert.That(() => new GlyssenShare(file.Path), Throws.InstanceOf<ArgumentException>());
+			}
+		}
+
+		[Test] public void Constructor_InvalidGlyssenshareFileNoProject_ThrowsArgumentException()
+		{
+			using (var file = TempFile.FromResource(Properties.Resources.qaa_Bogus, kShareFileExtension))
+			{
+				Assert.That(file.Path, Does.Exist, "SETUP - Sanity check");
+				Assert.That(() => new GlyssenShare(file.Path), Throws.InstanceOf<ArgumentException>());
+			}
+		}
+
+		[Test] public void Constructor_ValidGlyssenshareFile_SetsProjectFilePathCorrectly()
 		{
 			using (var file = TempFile.FromResource(Properties.Resources.qaa_Elbonian, kShareFileExtension))
 			{
-				Assert.That(file.Path, Does.Not.Exist, "SETUP - Sanity check");
+				Assert.That(file.Path, Does.Exist, "SETUP - Sanity check");
 				var glyssenShare = new GlyssenShare(file.Path);
-				Assert.That(glyssenShare.ProjectFilePath, Does.EndWith(@"\qaa\1af90518e2ccf03b\Elbonian"));
+				Assert.That(glyssenShare.ProjectFilePath,
+					Does.EndWith(Combine("qaa", "1af90518e2ccf03b", "Elbonian", "qaa.glyssen")));
+			}
+		}
+
+		[Test] public void Constructor_ManualGlyssenshareFileWithMultipleDirEntries_SetsProjectFilePathCorrectly()
+		{
+			using (var file = TempFile.FromResource(Properties.Resources.manual, kShareFileExtension))
+			{
+				Assert.That(file.Path, Does.Exist, "SETUP - Sanity check");
+				var glyssenShare = new GlyssenShare(file.Path);
+				Assert.That(glyssenShare.ProjectFilePath,
+					Does.EndWith(Combine("qaa", "1af90518e2ccf03b", "Elbonian", "qaa.glyssen")));
+			}
+		}
+
+		[Test] public void Constructor_ManualGlyssenshareFileWithNoDirEntries_SetsProjectFilePathCorrectly()
+		{
+			using (var file = TempFile.FromResource(Properties.Resources.nodirentries, kShareFileExtension))
+			{
+				Assert.That(file.Path, Does.Exist, "SETUP - Sanity check");
+				var glyssenShare = new GlyssenShare(file.Path);
+				Assert.That(glyssenShare.ProjectFilePath,
+					Does.EndWith(Combine("qaa", "1af90518e2ccf03b", "Elbonian", "qaa.glyssen")));
+			}
+		}
+
+		internal class ProjectStub : IUserProject
+		{
+			public string Name { get; }
+			public string LanguageIsoCode { get; }
+			public string ValidLanguageIsoCode => LanguageIsoCode;
+			public string MetadataId { get; }
+			public string FontFamily => "Charis SIL";
+
+			internal ProjectStub()
+			{
+				LanguageIsoCode = "qaa";
+				MetadataId = "not used";
+				Name = "Elbonian";
 			}
 		}
 	}
